a38df9354072e867718e4971727b44d58941b26e
benoitlub <benoitlubert@gmail.com>
2026-06-22 13:14:17 +0200

    Configure Vite pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/le-bazar-feuch/",
   server: {
     host: "::",
     port: 8080,
